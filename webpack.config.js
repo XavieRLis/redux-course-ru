69d4a46b36a4622d919b67975685f2c8d8a952db
xavier.lis <xavier.lis@gmail.com>
2016-07-15 14:43:43 +0300

    Add ESLint and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -16,9 +17,18 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new NpmInstallPlugin() // <--
     ],
     module: { //Обновлено
+        preLoaders: [ //добавили ESlint в preloaders
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+            }
+        ],
         loaders: [ //добавили babel-loader
             {
                 loaders: ['react-hot', 'babel-loader'],
